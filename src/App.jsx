aad3fc45000551ce422f1b7fd0c08cf394613e72
Abhijeet1005 <aschauhan1052002@gmail.com>
2025-05-13 13:20:38 +0530

    feat: add dependencies for Solana and cryptographic functions
    - Added @solana/web3.js, ed25519-hd-key, and tweetnacl to dependencies in package.json.
    - Introduced a new CSS class .small-faded to App.css for styling.
    - Updated App.jsx to include a button for copying the generated mnemonic to clipboard with an alert notification.

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,23 @@ function App() {
     const [mnemonic, setMnemonic] = useState("");
   return (
     <>
-    <button onClick={async function(){
+    <button className='card' onClick={async function(){
       const mn = await generateMnemonic();
       setMnemonic(mn)
-      console.log(mn)
+      console.log(mnemonic);
     }}>Click to generate seed phrase</button>
+
+    <p className="read-the-docs">{mnemonic}
+      <br />
+    <button className='small-faded'
+      onClick={() => {
+        navigator.clipboard.writeText(mnemonic);
+        alert("Copied to clipboard!");
+      }}
+      >
+        Copy
+    </button>
+    </p>
     </>
   )
 }
